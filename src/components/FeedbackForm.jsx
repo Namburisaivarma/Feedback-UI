92655ccd8c007f092f7a3b7ec8045c950f0fe69c
Namburisaivarma <saivarma.shanmukha@gmail.com>
2023-12-06 17:54:38 +0530

    Added a RatingSelect Component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,30 +1,32 @@
-import Card from './shared/Card'
-import { useState } from 'react'
-import Button from './shared/Button'
+import Card from './shared/Card';
+import { useState } from 'react';
+import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
 
 function FeedbackForm() {
-  const [text, setText] = useState('')
-  const [btnDisabled, setBtnDisabled] = useState(true)
-  const [message, setMessage] = useState('')
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (e) => {
     if (text == '') {
-      setBtnDisabled(true)
-      setMessage(null)
+      setBtnDisabled(true);
+      setMessage(null);
     } else if (text !== '' && text.trim().length <= 10) {
-      setMessage('Text must be atleast 10 characters')
-      setBtnDisabled(true)
+      setMessage('Text must be atleast 10 characters');
+      setBtnDisabled(true);
     } else {
-      setMessage(null)
-      setBtnDisabled(false)
+      setMessage(null);
+      setBtnDisabled(false);
     }
-    setText(e.target.value)
-  }
+    setText(e.target.value);
+  };
   return (
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* { Rating Select} */}
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -39,7 +41,7 @@ function FeedbackForm() {
         {message && <div className="message">{message}</div>}
       </form>
     </Card>
-  )
+  );
 }
 
-export default FeedbackForm
+export default FeedbackForm;
