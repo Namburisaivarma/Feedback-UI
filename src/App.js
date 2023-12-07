303709a0c8c603b6105c0df06f00c850bc6bf881
Namburisaivarma <saivarma.shanmukha@gmail.com>
2023-12-07 12:25:13 +0530

    Implemented the routing concept for the react app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import { v4 as uuidv4 } from 'uuid';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,12 +23,29 @@ function App() {
   };
   return (
     <>
-      <Header />
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
+
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import Card from '../components/shared/Card';
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this Project</h1>
+        <p>This is a feedback app for a product or a service</p>
+
+        <p>
+          <a href="/">Back to home</a>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
