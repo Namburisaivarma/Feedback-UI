96d7727c77261f389d11baef2d55ffb00289ca47
Namburisaivarma <saivarma.shanmukha@gmail.com>
2023-12-07 16:12:42 +0530

    Implemented use context for couple of files for adding and deleting.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
-import { v4 as uuidv4 } from 'uuid';
+
 import AboutPage from './pages/AboutPage';
 import AboutIconLink from './components/AboutIconLink';
 import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
-
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
   return (
     <>
       <FeedbackProvider>
@@ -35,9 +21,9 @@ function App() {
                 path="/"
                 element={
                   <>
-                    <FeedbackForm handleAdd={addFeedback} />
+                    <FeedbackForm />
                     <FeedbackStats />
-                    <FeedbackList handleDelete={deleteFeedback} />
+                    <FeedbackList />
                   </>
                 }
               />
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,7 +2,7 @@ import FeedbackItem from './FeedbackItem';
 import { useContext } from 'react';
 import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackList({ handleDelete }) {
+function FeedbackList() {
   const { feedback } = useContext(FeedbackContext);
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback</p>;
@@ -10,7 +10,7 @@ function FeedbackList({ handleDelete }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem handleDelete={handleDelete} key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} />
       ))}
     </div>
   );
