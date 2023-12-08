babad6cb9f697915ae47097fff996cebfcb1c6f1
Namburisaivarma <saivarma.shanmukha@gmail.com>
2023-12-08 13:01:09 +0530

    Added update and delete fuctionality using the json server.

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -39,20 +39,30 @@ export const FeedbackProvider = ({ children }) => {
 
   //Delete Feedback
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   // Update Feedback
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await response.json();
     setFeedback(
       feedback.map((item) =>
         item.id === id
           ? {
               ...item,
-              ...updItem,
+              ...data,
             }
           : item
       )
